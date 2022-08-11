dd14e7c86fe25ac344a766bc5479df0fcfa4af22
Andy <andy8647lgc@gmail.com>
2022-08-11 14:55:56 -0400

    <feat> coffee module

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,49 @@
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { CoffeeEntity } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: CoffeeEntity[] = [
+    {
+      id: 1,
+      name: 'Cappuccino',
+      brand: 'Starbucks',
+      flavors: ['vanilla', 'caramel'],
+    },
+  ];
+
+  findAll(): CoffeeEntity[] {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((coffee) => coffee.id === Number(id));
+    if (!coffee) {
+      throw new NotFoundException(`Coffee with id ${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update
+    } else {
+      // todo
+    }
+  }
+
+  remove(id: string) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // remove
+      this.coffees.splice(this.coffees.indexOf(existingCoffee), 1);
+    } else {
+      // todo
+    }
+  }
+}
