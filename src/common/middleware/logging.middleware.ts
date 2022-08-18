4be8f1456b2486873f0d5fa7f106cb1155a97135
Andy <andy8647lgc@gmail.com>
2022-08-18 13:12:25 -0400

    <feat> add time logging to logging middleware

--- src/common/middleware/logging.middleware.ts
+++ src/common/middleware/logging.middleware.ts
@@ -3,7 +3,9 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('from logging middleware');
+    console.time('Request-response time');
+    console.log('Hi from middleware');
+    res.on('finish', () => console.timeEnd('Request-response time'));
     next(); // always call next() to pass the request to the next middleware in the chain
   }
 }
